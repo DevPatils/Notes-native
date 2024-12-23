31cf3d9b35924c9e753ae8328177d88d2a3b5ac3
Haard Solanki <haardsolanki.itm@gmail.com>
2024-12-23 22:55:49 +0530

    refactor: reorganize project structure and remove unused files; add global styles and theme hooks

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import '../global.css'
+const index = () => {
+  return (
+    <View className='bg-blue-500 h-full'>
+      <Text className='text-red-500 text-2xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
